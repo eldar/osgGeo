8634f15ffb1b5906d440c103aafd88dbb454c320
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2012-01-24 16:17:31 +0000

    Horizon3D: Parallelisation of tesseltation.

--- src/osgGeo/Horizon3D.cpp
+++ src/osgGeo/Horizon3D.cpp
@@ -26,6 +26,266 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 namespace osgGeo
 {
 
+class Horizon3DTesselator : public OpenThreads::Thread
+{
+public:
+    struct CommonData
+    {
+        CommonData(const Vec2i& fullSize_,
+                   osg::DoubleArray *depthVals_,
+                   float maxDepth_,
+                   const std::vector<osg::Vec2d> &coords_);
+
+        Vec2i fullSize; // full size of the horizon
+        osg::DoubleArray *depthVals;
+        float maxDepth;
+        std::vector<osg::Vec2d> coords;
+        Vec2i maxSize; // reference size of the tile
+        osg::Vec2d iInc, jInc; // increments of realworld coordinates along the grid dimensions
+        int numHTiles, numVTiles; // number of tiles of horizon within
+    };
+
+    /**
+      * This struct contains information that identifies a tile within horizon
+      * and which is used to build it.
+      */
+    struct Job
+    {
+        Job(int hI, int vI, int resLevel_) :
+            hIdx(hI), vIdx(vI), resLevel(resLevel_) {}
+
+        // indexes of the tile within the horizon
+        int hIdx, vIdx;
+
+        // resolution level of horizon 1, 2, 3 ... which means that every
+        // first, second, fourth ... points are displayed and all the rest
+        // are discarded
+        int resLevel;
+    };
+
+    struct Result
+    {
+        osg::ref_ptr<osg::Node> node;
+        int resLevel;
+    };
+
+    Horizon3DTesselator(const CommonData &data);
+
+    void addJob(const Job &job);
+    virtual void run();
+
+    bool isUndef(double val);
+
+    std::vector<Result> &getResults();
+
+private:
+    const CommonData &_data;
+    std::vector<Job> _jobs;
+    std::vector<Result> _results;
+};
+
+Horizon3DTesselator::CommonData::CommonData(const Vec2i& fullSize_,
+                                            osg::DoubleArray *depthVals_,
+                                            float maxDepth_,
+                                            const std::vector<osg::Vec2d> &coords_)
+{
+    fullSize = fullSize_;
+    depthVals = depthVals_;
+    maxDepth = maxDepth_;
+    coords = coords_;
+
+    maxSize = Vec2i(256, 256);
+
+    iInc = (coords[2] - coords[0]) / (fullSize.x() - 1);
+    jInc = (coords[1] - coords[0]) / (fullSize.y() - 1);
+
+    numHTiles = ceil(float(fullSize.x()) / maxSize.x());
+    numVTiles = ceil(float(fullSize.y()) / maxSize.y());
+}
+
+Horizon3DTesselator::Horizon3DTesselator(const CommonData &data) :
+    _data(data)
+{
+}
+
+void Horizon3DTesselator::addJob(const Job &job)
+{
+    _jobs.push_back(job);
+}
+
+bool Horizon3DTesselator::isUndef(double val)
+{
+    return val >= _data.maxDepth;
+}
+
+std::vector<Horizon3DTesselator::Result> &Horizon3DTesselator::getResults()
+{
+    return _results;
+}
+
+void Horizon3DTesselator::run()
+{
+    const CommonData &data = _data;
+
+    for(int jId = 0; jId < _jobs.size(); ++jId)
+    {
+        const Job &job = _jobs[jId];
+
+        // compression rate. 1 means no compression
+        const int compr = pow(2, job.resLevel);
+        int iCompr = compr;
+        int jCompr = compr;
+
+        int realHSize = data.maxSize.x() / iCompr;
+        int realVSize = data.maxSize.y() / jCompr;
+
+        const int hSize = job.hIdx < (data.numHTiles - 1) ?
+                    (realHSize + 1) : (data.fullSize.x() - data.maxSize.x() * (data.numHTiles - 1)) / iCompr;
+        const int vSize = job.vIdx < (data.numVTiles - 1) ?
+                    (realVSize + 1) : ((data.fullSize.y() - data.maxSize.y() * (data.numVTiles - 1))) / jCompr;
+
+        osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(hSize * vSize);
+
+        // first we construct an array of vertices which is just a grid
+        // of depth values.
+        for(int i = 0; i < hSize; ++i)
+            for(int j = 0; j < vSize; ++j)
+            {
+                const int iGlobal = job.hIdx * data.maxSize.x() + i * iCompr;
+                const int jGlobal = job.vIdx * data.maxSize.y() + j * jCompr;
+                osg::Vec2d hor = data.coords[0] + data.iInc * iGlobal + data.jInc * jGlobal;
+                (*vertices)[i*vSize+j] = osg::Vec3(
+                            hor.x(),
+                            hor.y(),
+                            data.depthVals->at(iGlobal*data.fullSize.y()+jGlobal)
+                            );
+            }
+
+        // the following loop populates array of indices that make up
+        // triangles out of vertices data, each grid cell has 2 triangles.
+        // If a vertex is undefined then triangle that contains it is
+        // discarded. Also normals are calculated for each triangle to be
+        // later used for calculating normals per vertex
+        osg::ref_ptr<osg::DrawElementsUInt> indices =
+                new osg::DrawElementsUInt(GL_TRIANGLES);
+
+        osg::ref_ptr<osg::Vec3Array> triangleNormals =
+                new osg::Vec3Array((hSize - 1) * (vSize - 1) * 2);
+
+        for(int i = 0; i < hSize - 1; ++i)
+            for(int j = 0; j < vSize - 1; ++j)
+            {
+                const int i00 = i*vSize+j;
+                const int i10 = (i+1)*vSize+j;
+                const int i01 = i*vSize+(j+1);
+                const int i11 = (i+1)*vSize+(j+1);
+
+                const osg::Vec3 v00 = (*vertices)[i00];
+                const osg::Vec3 v10 = (*vertices)[i10];
+                const osg::Vec3 v01 = (*vertices)[i01];
+                const osg::Vec3 v11 = (*vertices)[i11];
+
+                if(isUndef(v10.z()) || isUndef(v01.z()))
+                    continue;
+
+                // first triangle
+                if(!isUndef(v00.z()))
+                {
+                    indices->push_back(i00);
+                    indices->push_back(i10);
+                    indices->push_back(i01);
+                }
+
+                // second triangle
+                if(!isUndef(v11.z()))
+                {
+                    indices->push_back(i10);
+                    indices->push_back(i01);
+                    indices->push_back(i11);
+                }
+
+                // calculate triangle normals
+                osg::Vec3 norm1 = (v01 - v00) ^ (v10 - v00);
+                norm1.normalize();
+                (*triangleNormals)[(i*(vSize-1)+j)*2] = norm1;
+
+                osg::Vec3 norm2 = (v10 - v11) ^ (v01 - v11);
+                norm2.normalize();
+                (*triangleNormals)[(i*(vSize-1)+j)*2+1] = norm2;
+            }
+
+        // The following loop calculates normals per vertex. Because
+        // each vertex might be shared between many triangles(up to 6)
+        // we find out which triangles this particular vertex is shared
+        // and then compute the average of normals per triangle.
+        osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(hSize * vSize);
+
+        osg::Vec3 triNormCache[6];
+
+        for(int i = 0; i < hSize; ++i)
+            for(int j = 0; j < vSize; ++j)
+            {
+                int k = 0;
+                for(int l = 0; l < k; ++l)
+                    triNormCache[l] = osg::Vec3();
+
+                if((i < hSize - 1) && (j < vSize - 1))
+                {
+                    triNormCache[k++] = (*triangleNormals)[(i*(vSize-1)+j)*2];
+                }
+
+                if(i > 0 && j < vSize - 1)
+                {
+                    triNormCache[k++] = (*triangleNormals)[((i-1)*(vSize-1)+j)*2];
+                    triNormCache[k++] = (*triangleNormals)[((i-1)*(vSize-1)+j)*2+1];
+                }
+
+                if(j > 0 && i < hSize - 1)
+                {
+                    triNormCache[k++] = (*triangleNormals)[(i*(vSize-1)+j-1)*2];
+                    triNormCache[k++] = (*triangleNormals)[(i*(vSize-1)+j-1)*2+1];
+                }
+
+                if(i > 0 && j > 0)
+                {
+                    triNormCache[k++] = (*triangleNormals)[((i-1)*(vSize-1)+j-1)*2+1];
+                }
+
+                if(k > 0)
+                {
+                    osg::Vec3 norm;
+                    for(int l = 0; l < k; ++l)
+                        norm += triNormCache[l];
+
+                    norm.normalize();
+
+                    (*normals)[i*vSize+j] = -norm;
+                }
+            }
+
+        osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
+        colors->push_back(osg::Vec4(1.0f, 0.0f, 1.0f, 1.0f));
+
+        osg::ref_ptr<osg::Geometry> quad = new osg::Geometry;
+        quad->setVertexArray(vertices.get());
+
+        quad->setNormalArray(normals.get());
+        quad->setNormalBinding(osg::Geometry::BIND_PER_VERTEX);
+        quad->setColorArray(colors.get());
+        quad->setColorBinding(osg::Geometry::BIND_OVERALL);
+
+        quad->addPrimitiveSet(indices.get());
+
+        osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+        geode->addDrawable(quad.get());
+
+        Result result;
+        result.node = geode;
+        result.resLevel = job.resLevel;
+        _results.push_back(result);
+    }
+}
+
 Horizon3DNode::Horizon3DNode() :
   Group()
 {
@@ -33,7 +293,7 @@ Horizon3DNode::Horizon3DNode() :
 }
 
 Horizon3DNode::Horizon3DNode(const Horizon3DNode& other,
-              const osg::CopyOp& op) :
+                             const osg::CopyOp& op) :
     Group(other, op)
 {
 
@@ -75,195 +335,58 @@ std::vector<osg::Vec2d> Horizon3DNode::getCornerCoords() const
     return _cornerCoords;
 }
 
-void Horizon3DNode::createTiles(int resLevel)
+void Horizon3DNode::updateDrawables()
 {
     if(getDepthArray()->getType() != osg::Array::DoubleArrayType)
         return;
 
-    // compression rate. 1 means no compression
-    const int compr = pow(2, resLevel);
-    const int iCompr = compr;
-    const int jCompr = compr;
+    Horizon3DTesselator::CommonData data(getSize(),
+                                         dynamic_cast<osg::DoubleArray*>(getDepthArray()),
+                                         getMaxDepth(),
+                                         getCornerCoords());
 
-    const osg::DoubleArray &depthVals =
-            *(dynamic_cast<osg::DoubleArray*>(getDepthArray()));
+    const int numCPUs = OpenThreads::GetNumberOfProcessors();
+    std::vector<Horizon3DTesselator*> threads(numCPUs);
+    for(int i = 0; i < numCPUs; ++i)
+        threads[i] = new Horizon3DTesselator(data);
 
-    const int allHSize = getSize().x();
-    const int allVSize = getSize().y();
-
-    const int maxHSize = 256;
-    const int maxVSize = 256;
-
-    const std::vector<osg::Vec2d> coords = getCornerCoords();
-    const osg::Vec2d iInc = (coords[2] - coords[0]) / (allHSize - 1);
-    const osg::Vec2d jInc = (coords[1] - coords[0]) / (allVSize - 1);
-
-    const int numHTiles = ceil(float(allHSize) / maxHSize);
-    const int numVTiles = ceil(float(allVSize) / maxVSize);
-
-    const int realHSize = maxHSize / iCompr;
-    const int realVSize = maxVSize / jCompr;
-
-    for(int hIdx = 0; hIdx < numHTiles; ++hIdx)
-    {
-        const int hSize = hIdx < (numHTiles - 1) ?
-                    (realHSize + 1) : (allHSize - maxHSize * (numHTiles - 1)) / iCompr;
-        for(int vIdx = 0; vIdx < numVTiles; ++vIdx)
-        {
-            const int vSize = vIdx < (numVTiles - 1) ?
-                        (realVSize + 1) : ((allVSize - maxHSize * (numVTiles - 1))) / jCompr;
-
-            osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(hSize * vSize);
-
-            // first we construct an array of vertices which is just a grid
-            // of depth values.
-            for(int i = 0; i < hSize; ++i)
-                for(int j = 0; j < vSize; ++j)
-                {
-                    const int iGlobal = hIdx * maxHSize + i * iCompr;
-                    const int jGlobal = vIdx * maxVSize + j * jCompr;
-                    osg::Vec2d hor = coords[0] + iInc * iGlobal + jInc * jGlobal;
-                    (*vertices)[i*vSize+j] = osg::Vec3(
-                                hor.x(),
-                                hor.y(),
-                                depthVals[iGlobal*allVSize+jGlobal]
-                                );
-                }
-
-            // the following loop populates array of indices that make up
-            // triangles out of vertices data, each grid cell has 2 triangles.
-            // If a vertex is undefined then triangle that contains it is
-            // discarded. Also normals are calculated for each triangle to be
-            // later used for calculating normals per vertex
-            osg::ref_ptr<osg::DrawElementsUInt> indices =
-                    new osg::DrawElementsUInt(GL_TRIANGLES);
-
-            osg::ref_ptr<osg::Vec3Array> triangleNormals =
-                    new osg::Vec3Array((hSize - 1) * (vSize - 1) * 2);
-
-            for(int i = 0; i < hSize - 1; ++i)
-                for(int j = 0; j < vSize - 1; ++j)
-                {
-                    const int i00 = i*vSize+j;
-                    const int i10 = (i+1)*vSize+j;
-                    const int i01 = i*vSize+(j+1);
-                    const int i11 = (i+1)*vSize+(j+1);
-
-                    const osg::Vec3 v00 = (*vertices)[i00];
-                    const osg::Vec3 v10 = (*vertices)[i10];
-                    const osg::Vec3 v01 = (*vertices)[i01];
-                    const osg::Vec3 v11 = (*vertices)[i11];
-
-                    if(isUndef(v10.z()) || isUndef(v01.z()))
-                        continue;
-
-                    // first triangle
-                    if(!isUndef(v00.z()))
-                    {
-                        indices->push_back(i00);
-                        indices->push_back(i10);
-                        indices->push_back(i01);
-                    }
-
-                    // second triangle
-                    if(!isUndef(v11.z()))
-                    {
-                        indices->push_back(i10);
-                        indices->push_back(i01);
-                        indices->push_back(i11);
-                    }
-
-                    // calculate triangle normals
-                    osg::Vec3 norm1 = (v01 - v00) ^ (v10 - v00);
-                    norm1.normalize();
-                    (*triangleNormals)[(i*(vSize-1)+j)*2] = norm1;
-
-                    osg::Vec3 norm2 = (v10 - v11) ^ (v01 - v11);
-                    norm2.normalize();
-                    (*triangleNormals)[(i*(vSize-1)+j)*2+1] = norm2;
-                }
-
-            // The following loop calculates normals per vertex. Because
-            // each vertex might be shared between many triangles(up to 6)
-            // we find out which triangles this particular vertex is shared
-            // and then compute the average of normals per triangle.
-            osg::ref_ptr<osg::Vec3Array> normals = new osg::Vec3Array(hSize * vSize);
-
-            osg::Vec3 triNormCache[6];
-
-            for(int i = 0; i < hSize; ++i)
-                for(int j = 0; j < vSize; ++j)
-                {
-                    int k = 0;
-                    for(int l = 0; l < k; ++l)
-                        triNormCache[l] = osg::Vec3();
-
-                    if((i < hSize - 1) && (j < vSize - 1))
-                    {
-                        triNormCache[k++] = (*triangleNormals)[(i*(vSize-1)+j)*2];
-                    }
-
-                    if(i > 0 && j < vSize - 1)
-                    {
-                        triNormCache[k++] = (*triangleNormals)[((i-1)*(vSize-1)+j)*2];
-                        triNormCache[k++] = (*triangleNormals)[((i-1)*(vSize-1)+j)*2+1];
-                    }
-
-                    if(j > 0 && i < hSize - 1)
-                    {
-                        triNormCache[k++] = (*triangleNormals)[(i*(vSize-1)+j-1)*2];
-                        triNormCache[k++] = (*triangleNormals)[(i*(vSize-1)+j-1)*2+1];
-                    }
-
-                    if(i > 0 && j > 0)
-                    {
-                        triNormCache[k++] = (*triangleNormals)[((i-1)*(vSize-1)+j-1)*2+1];
-                    }
-
-                    if(k > 0)
-                    {
-                        osg::Vec3 norm;
-                        for(int l = 0; l < k; ++l)
-                            norm += triNormCache[l];
-
-                        norm.normalize();
-
-                        (*normals)[i*vSize+j] = -norm;
-                    }
-                }
-
-            osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
-            colors->push_back(osg::Vec4(1.0f, 0.0f, 1.0f, 1.0f));
-
-            osg::ref_ptr<osg::Geometry> quad = new osg::Geometry;
-            quad->setVertexArray(vertices.get());
-
-            quad->setNormalArray(normals.get());
-            quad->setNormalBinding(osg::Geometry::BIND_PER_VERTEX);
-            quad->setColorArray(colors.get());
-            quad->setColorBinding(osg::Geometry::BIND_OVERALL);
-
-            quad->addPrimitiveSet(indices.get());
-
-            osg::ref_ptr<osg::Geode> geode = new osg::Geode;
-            geode->addDrawable(quad.get());
-            addChild(geode.get());
-            _nodes[resLevel].push_back(geode.get());
-        }
-    }
-}
-
-void Horizon3DNode::updateDrawables()
-{
     const int resolutionsNum = 3;
     _nodes.resize(resolutionsNum);
-    for(int i = 0; i < resolutionsNum; ++i)
-        createTiles(i);
-}
 
-bool Horizon3DNode::isUndef(double val)
-{
-    return val >= _maxdepth;
+    int currentThread = 0;
+    for(int resLevel = 0; resLevel < resolutionsNum; ++resLevel)
+    {
+        for(int hIdx = 0; hIdx < data.numHTiles; ++hIdx)
+        {
+            for(int vIdx = 0; vIdx < data.numVTiles; ++vIdx)
+            {
+                threads[currentThread]->addJob(Horizon3DTesselator::Job(hIdx, vIdx, resLevel));
+                currentThread++;
+                if(currentThread == numCPUs)
+                    currentThread = 0;
+            }
+        }
+    }
+
+    for(int i = 0; i < numCPUs; ++i)
+        threads[i]->startThread();
+
+    for(int i = 0; i < numCPUs; ++i)
+        threads[i]->join();
+
+    for(int i = 0; i < numCPUs; ++i)
+    {
+        std::vector<Horizon3DTesselator::Result> &nodes = threads[i]->getResults();
+        for(int j = 0; j < nodes.size(); ++j)
+        {
+            Horizon3DTesselator::Result res = nodes[j];
+            addChild(res.node.get());
+            _nodes[res.resLevel].push_back(res.node.get());
+        }
+    }
+
+    for(int i = 0; i < numCPUs; ++i)
+        delete threads[i];
 }
 
 void Horizon3DNode::setMaxDepth(float val)
